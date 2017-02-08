310dfa4edff3d8a1723826f842107e9d3c44960d
Spacevash <nidhi.gaday@gmail.com>
2017-02-08 12:23:53 -0500

    update

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ function loadData() {
 
 
     var NYTapiKey = config.NYTapiKey;
-    var NYTurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + cityStr + "&sort=newest&api-key=" + NYTapiKey + ";
+    var NYTurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + cityStr + "&sort=newest&api-key=" + NYTapiKey;
 
     $nytHeaderElem.text('New York Times Articles About ' + cityStr);
 
