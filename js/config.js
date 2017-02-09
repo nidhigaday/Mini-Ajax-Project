6570be462d235a5742d31f8744da8de2ebdf9e42
Spacevash <nidhi.gaday@gmail.com>
2017-02-09 17:41:50 -0500

    Test

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-/*var config = {
+var config = {
     apiKey: "jsdknvilvbaslvsjdk",
     NYTapiKey: "sdiivjfiuvdfklvfjnldf",
     Consumer_Key: "hvepirfdhvkvjfpvuifb",
@@ -16,25 +16,7 @@ var parameters = {
 
 var consumer_Secret = config.Consumer_Secret, // unique key to each api account - must include
     token_Secret = config.Token_Secret;
-*/
 
 
 
-var config = {
-    apiKey: "AIzaSyD5Et3G-0OOF0B4ER5zHiM19kZo2xXfdO4",
-    NYTapiKey: "e15d65400bed43469eb44fd08e475b3a",
-    Consumer_Key: "ozZGFps-eIx-vXl1ROf4hA",
-    Consumer_Secret: "0pUUUKeY3j1P88bz55nr7LN1J5w",
-    Token: "oZ7hinuN_4xr5hmrNw8k-SFBGYNdFdHj",
-    Token_Secret: "Z5OdBXDGliNWv9KrtLfr7XPYujQ"
-};
-//var NYTapiKey = "3d20415456d2153f63e3b1c268f9d940:5:73617106";
-var apiKey = config.apiKey,
-    NYTapiKey = config.NYTapiKey;
-var parameters = {
-    oauth_consumer_key: config.Consumer_Key,
-    oauth_token: config.Token,
-};
 
-var consumer_Secret = config.Consumer_Secret, // unique key to each api account - must include
-    token_Secret = config.Token_Secret;
