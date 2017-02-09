80cceb06116648d633eefc0b211a5d300146cd38
Spacevash <nidhi.gaday@gmail.com>
2017-02-09 17:43:27 -0500

    test2

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ var config = {
     Token: "ioihnpgeirhfvlifdv",
     Token_Secret: "hgiuergboeudjhgrbdf"
 };
-//var NYTapiKey = "3d20415456d2153f63e3b1c268f9d940:5:73617106";
+//var NYTapiKey = "3d204154fgvbhnjnbhgvftcdrctfvygubh617106";
 var apiKey = config.apiKey,
     NYTapiKey = config.NYTapiKey;
 var parameters = {
