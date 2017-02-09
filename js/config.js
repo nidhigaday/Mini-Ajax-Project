494b3939b5cddec3bfd6dedb583fd05a4c988da9
nidhigaday <nidhi.gaday@gmail.com>
2017-02-09 17:27:56 -0500

    Create a safety hideout for my keys

---
+++ js/config.js
@@ -0,0 +1,18 @@
+var config = {
+    apiKey: "jsdknvilvbaslvsjdk",
+    NYTapiKey: "sdiivjfiuvdfklvfjnldf",
+    Consumer_Key: "hvepirfdhvkvjfpvuifb",
+    Consumer_Secret: "cs8j9dchebpiuvd78",
+    Token: "ioihnpgeirhfvlifdv",
+    Token_Secret: "hgiuergboeudjhgrbdf"
+};
+//var NYTapiKey = "3d20415456d2153f63e3b1c268f9d940:5:73617106";
+var apiKey = config.apiKey,
+    NYTapiKey = config.NYTapiKey;
+var parameters = {
+    oauth_consumer_key: config.Consumer_Key,
+    oauth_token: config.Token,
+};
+
+var consumer_Secret = config.Consumer_Secret, // unique key to each api account - must include
+    token_Secret = config.Token_Secret;
